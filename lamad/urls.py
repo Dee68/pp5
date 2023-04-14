b4a2bf223e4cbb6ba086066e27b5827c251053a1
Dee68 <ddimie283@gmail.com>
2023-04-14 10:11:31 +0000

    Add password reset functionality.

--- lamad/urls.py
+++ lamad/urls.py
@@ -17,9 +17,32 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('', include('home.urls')),
+    # ~~~~~~ password reset ~~~~~~~~~~~~~~~~~~~~~~
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/password_reset.html'
+    ), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='account/password_reset_sent.html'
+    ), name='password_reset_done'),
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='account/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+        ),
+    path(
+        'password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='account/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+        ),
+    # ~~~~~~~~~~~~~ end password reset ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

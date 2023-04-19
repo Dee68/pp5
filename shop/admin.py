1e2aa2556f6a74625f73cc67f860c1addfb44b8b
Dee68 <ddimie283@gmail.com>
2023-04-19 17:24:43 +0000

    Add review and rating models with styles.

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Review, Rating
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,5 +19,5 @@ class ProductAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
-# admin.site.register(Review)
-# admin.site.register(Rating)
+admin.site.register(Review)
+admin.site.register(Rating)

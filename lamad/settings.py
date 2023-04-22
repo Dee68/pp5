7f40047f312b06212fc4317f36f1e113148f62c9
Dee68 <ddimie283@gmail.com>
2023-04-22 22:19:46 +0000

    Add custom authentication backend for social login.

--- lamad/settings.py
+++ lamad/settings.py
@@ -99,6 +99,7 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.facebook.FacebookOAuth2',
     'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
+    'account.auth_backends.Emailbackend',
 
 ]
 
